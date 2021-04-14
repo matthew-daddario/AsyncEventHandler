cb85ee1c4841c87628c88bccf3e3b782de791ee2
Matthew D'Addario <matt.daddario@hotmail.com>
2021-04-14 13:05:32 -0400

    Initial commit

---
+++ AsyncEventHandler/AsyncEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncEventHandler
+{
+    public delegate Task AsyncEventHandler();
+
+    public delegate Task AsyncEventHandler<in T>(T eventArgs);
+
+    public static class AsyncEventHandlerExtensions
+    {
+        public static Task InvokeAndWaitAsync(this AsyncEventHandler eventHandler)
+        {
+            if (eventHandler == null) return Task.CompletedTask;
+
+            var delegates = eventHandler.GetInvocationList().Cast<AsyncEventHandler>();
+            var tasks = delegates.Select(it => it.Invoke());
+
+            return Task.WhenAll(tasks);
+        }
+
+        public static Task InvokeAndWaitAsync<T>(this AsyncEventHandler<T> eventHandler, T eventArgs)
+        {
+            if (eventHandler == null) return Task.CompletedTask;
+
+            var delegates = eventHandler.GetInvocationList().Cast<AsyncEventHandler<T>>();
+            var tasks = delegates.Select(it => it.Invoke(eventArgs));
+
+            return Task.WhenAll(tasks);
+        }
+
+        public static void InvokeAndWait(this AsyncEventHandler eventHandler) => InvokeAndWaitAsync(eventHandler).Wait();
+
+        public static void InvokeAndWait<T>(this AsyncEventHandler<T> eventHandler, T eventArgs) => InvokeAndWaitAsync(eventHandler, eventArgs).Wait();
+    }
+}
---
+++ AsyncEventHandler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncEventHandler
+{
+    class Program
+    {
+        static event AsyncEventHandler MyEvent;
+
+        static async Task Main(string[] _)
+        {
+            MyEvent += OnMyEvent;
+            await MyEvent.InvokeAndWaitAsync();
+            Console.WriteLine("All delegates have completed.");
+
+            Console.ReadLine();
+        }
+
+        private static async Task OnMyEvent()
+        {
+            Console.WriteLine("Delegate is invoked");
+            await Task.Delay(5_000);
+            Console.WriteLine("Async operation in delegate is completed");
+        }
+    }
+}
